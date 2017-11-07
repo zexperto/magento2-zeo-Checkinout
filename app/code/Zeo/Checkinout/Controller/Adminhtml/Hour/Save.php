7150047997cc9de653f105953a5b9c8da0b6adb9
wisam hakim <zexperto@hotmail.com>
2017-11-07 15:42:48 -0500

    fix save admin

--- app/code/Zeo/Checkinout/Controller/Adminhtml/Hour/Save.php
+++ app/code/Zeo/Checkinout/Controller/Adminhtml/Hour/Save.php
@@ -4,11 +4,7 @@ namespace Zeo\Checkinout\Controller\Adminhtml\Hour;
     
 class Save extends \Zeo\Checkinout\Controller\Adminhtml\Hour
 {
-    function decimalHours($time)
-    {
-        $hms = explode(":", $time);
-        return ($hms[0] + ($hms[1]/60) + ($hms[2]/3600));
-    }
+  
     public function execute()
     {
         if ($this->getRequest()->getPostValue()) {
@@ -40,13 +36,9 @@ class Save extends \Zeo\Checkinout\Controller\Adminhtml\Hour
               
                 $total_time = $timeFormat;
                                 
-                $decimalHours = $this->decimalHours($total_time);
-                
-                $decimalHours= ceil($decimalHours*1000)/1000;;
-              //  $decimalHours= round($decimalHours/ 0.05) * 0.05;
                 
                 $data["total_time"] = $total_time;
-                $data["total"] = $decimalHours;
+                $data["total"] = $total_times_seconds;
                 
                 $model->setData($data);
                 $session = $this->_objectManager->get('Magento\Backend\Model\Session');
